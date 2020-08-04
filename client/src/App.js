fbfc9bfce615a32455bbdfd8baf5d6d67652bf04
Ahmet Efeoglu <ahmetfatih325@hotmail.com>
2020-08-04 17:05:57 -0300

    Added the components

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,157 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <a href="index.html">haligone</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a className="signupbutton" href="signin.html">Sign in</a>
+        </div>
       </header>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/y.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+            <li>
+              <div className="product">
+                <img className="product-image" src="images/d1.jpg" alt="product" />
+                <div className="product-name">
+                  <a href="product.html">Slim Shirt</a>
+                </div>
+                <div className="product-brand">Nike</div>
+                <div className="product-price">$60</div>
+                <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        All rights reserved to Haligone Inc, 2020.
+      </footer>
     </div>
   );
 }
